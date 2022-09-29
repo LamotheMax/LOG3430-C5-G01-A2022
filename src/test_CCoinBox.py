78e2b6e39c2ca5eb350790d3d7ca954eb7291222
GoldenAlpharex <jerego1234@hotmail.com>
2022-09-29 09:40:15 -0400

    Added a test for ajouter_25c

--- src/test_CCoinBox.py
+++ src/test_CCoinBox.py
@@ -5,4 +5,15 @@ class Test_CCoinBox(unittest.TestCase):
 
     def test_something(self):
         self.assertTrue(True)
+    
+    def test_ajouter_25c_vente_permise(self):
+        coinbox = CCoinBox()
+
+        coinbox.ajouter_25c()
+
+        self.assertFalse(coinbox.vente_permise)
+
+        coinbox.ajouter_25c()
+
+        self.assertTrue(coinbox.vente_permise)
 
