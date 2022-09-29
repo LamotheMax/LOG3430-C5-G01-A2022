9e5185b941d0b3e56bae832a2bb72a3251f671bb
Sebastien Goll <50230837+SebGoll@users.noreply.github.com>
2022-09-29 15:40:21 +0200

    Test for bug 1

--- src/test_CCoinBox.py
+++ src/test_CCoinBox.py
@@ -5,4 +5,12 @@ class Test_CCoinBox(unittest.TestCase):
 
     def test_something(self):
         self.assertTrue(True)
+        
+    def test_add50():
+        cb = CCoinBox()
+        cb.ajouter_25c()
+        cb.ajouter_25c()
+        self.assertTrue(cb.get_monnaie_totale()==0)
+        self.assertTrue(cb.get_monnaie_courante()==2)
+        self.assertTrue(cb.get_vente_permise())
 
