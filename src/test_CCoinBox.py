9771d2f1b7483ad8fb853752a4e5779f1d08c113
GoldenAlpharex <jerego1234@hotmail.com>
2022-09-29 10:10:57 -0400

    Even more tests

--- src/test_CCoinBox.py
+++ src/test_CCoinBox.py
@@ -3,17 +3,38 @@ from CCoinBox import CCoinBox
 
 class Test_CCoinBox(unittest.TestCase):
 
-    def test_something(self):
-        self.assertTrue(True)
-    
     def test_ajouter_25c_vente_permise(self):
+        coinbox = CCoinBox()
+        coinbox.monnaie_courante = 1
+
+        coinbox.ajouter_25c()
+
+        self.assertTrue(coinbox.vente_permise)
+
+
+    def test_ajouter_25c_vente_interdite(self):
         coinbox = CCoinBox()
 
         coinbox.ajouter_25c()
 
         self.assertFalse(coinbox.vente_permise)
 
+
+    def test_ajouter_25c_ajoute_une_seule_piece(self):
+        coinbox = CCoinBox()
+
+        coinbox.monnaie_courante = 0
+
         coinbox.ajouter_25c()
 
-        self.assertTrue(coinbox.vente_permise)
+        self.assertEqual(coinbox.monnaie_courante, 1)
 
+
+    def test_retourne_monnaie_vente_interdite(self):
+        coinbox = CCoinBox()
+
+        coinbox.vente_permise = False
+
+        coinbox.retourne_monnaie()
+
+        self.assertFalse(coinbox.vente_permise)
