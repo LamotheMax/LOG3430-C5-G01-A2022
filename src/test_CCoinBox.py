7193249d6441fcdad8acfe6f479c8a0369f882c4
MLamothe <maxime.lamothe@polymtl.ca>
2022-09-29 09:57:33 -0400

    Test pour detecter bug + bugfix

--- src/test_CCoinBox.py
+++ src/test_CCoinBox.py
@@ -6,3 +6,8 @@ class Test_CCoinBox(unittest.TestCase):
     def test_something(self):
         self.assertTrue(True)
 
+    def test_add50(self):
+        cb = CCoinBox()
+        cb.monnaie_courante = 1
+        cb.ajouter_25c()
+        self.assertEqual(True, cb.get_vente_permise())
